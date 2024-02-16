a136ef3825149986ea7e0bbfe87088c3712b9caf
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-02-15 21:46:18 -0500

    something like that

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const http = require("node:http");
 const { createBareServer } = require("@tomphttp/bare-server-node");
 const fs = require("fs");
 const path = require("path");
+const url = require('url');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
 
 // Create an HTTP server
 const httpServer = http.createServer();
@@ -10,35 +15,21 @@ const bareServer = createBareServer("/bare/");
 httpServer.on("request", (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
-  } else if (req.url === "/") {
-    // Serve the index.html file when the root URL is requested
-    const indexPath = path.join(__dirname, "index.html");
-
-    fs.readFile(indexPath, "utf8", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    });
   } else {
-    res.writeHead(400, { "Content-Type": "text/plain" });
-    res.end("Not found.");
-  }
-});
+    app(req, res);
+  }});
 
-httpServer.on("upgrade", (req, socket, head) => {
+  httpServer.on("upgrade", (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
-});
+})
 
 httpServer.on("listening", () => {
   console.log("HTTP server listening");
+  console.log("View your server at localhost:2100")
 });
 
 httpServer.listen({
