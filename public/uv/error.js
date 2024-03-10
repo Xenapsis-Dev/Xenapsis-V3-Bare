2575d52700c244cf158cf48eb9f306e86c0ea7ed
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-03-10 00:34:41 -0500

    fixing stuff

--- public/uv/error.js
+++ public/uv/error.js
@@ -3,10 +3,6 @@ const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 const registerButton = document.getElementById("uv-register-sw");
 
-if (location.pathname.startsWith(__uv$config.prefix)) {
-  error.textContent = "Error: The service worker is not registered.";
-  registerButton.classList.add("show");
-}
 
 registerButton.addEventListener("click", async () => {
   try {
