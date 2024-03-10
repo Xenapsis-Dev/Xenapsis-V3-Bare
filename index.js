a04af8e00b499b3b5b92b533bf361b90825c6e30
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-03-09 22:15:39 -0500

    small update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const http = require("node:http");
-const { createBareServer } = require("@tomphttp/bare-server-node");
-const fs = require("fs");
-const path = require("path");
-const url = require('url');
-const express = require('express');
+import http from "node:http";
+import { createBareServer } from "@tomphttp/bare-server-node";
+import fs from "fs";
+import path, { join } from "path";
+import url from 'url';
+import express from 'express';
+import { fileURLToPath } from "url";
+
 const app = express();
 
 app.use(express.static('public'));
+
+const publicPath = fileURLToPath(new URL("/public/", import.meta.url));
 
 // Create an HTTP server
 const httpServer = http.createServer();
@@ -17,25 +21,25 @@ app.use((req, res) => {
   res.sendFile(join(publicPath, "404.html"));
 });
 
-
 httpServer.on("request", (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
   } else {
     app(req, res);
-  }});
+  }
+});
 
-  httpServer.on("upgrade", (req, socket, head) => {
+httpServer.on("upgrade", (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
-})
+});
 
 httpServer.on("listening", () => {
   console.log("HTTP server listening");
-  console.log("View your server at http://localhost:2100")
+  console.log("View your server at http://localhost:2100");
 });
 
 httpServer.listen({
