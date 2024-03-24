e7fbe978db996cbcfacf1c8bd20a9442a13c148a
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-03-24 10:47:44 -0400

    nav bar updates

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,11 @@
 var nav = ` 
 <li class="navlogo"><img class="navimgg" src="/imgs/navicons/favicon.png"><p1><p1>ㅤ</p1></p1><p1 class="navtextt"> Xenapsis</p1></li></a>
 <ul class="navbar">
-    <a class="navbutton" href="/index.html"><li><p1>ㅤ</p1><img id="home" class="navimg" src=""> home</li></a>
-    <a class="navbutton" href="/games.html"><li><p1>ㅤ</p1><img id="games" class="navimg" src=""> Games</li></a>
-    <a class="navbutton" href="/apps.html"><li><p1>ㅤ</p1><img id="apps" class="navimg" src=""> apps</li></a>
-    <a class="navbutton" href="/proxy.html"><li><p1>ㅤ</p1><img id="proxy" class="navimg" src=""> Proxy</li></a>
-    <a class="navbutton" href="/settings.html"><li><p1>ㅤ</p1><img id="settings" class="navimg" src=""> Settings</li></a>
+    <a class="navbutton" href="/index.html"><li> <img id="home" class="navimg" src=""><p1>ㅤ</p1><p1> home</p1></li></a>
+    <a class="navbutton" href="/games.html"><li> <img id="games" class="navimg" src=""><p1>ㅤ</p1><p1> Games</p1></li></a>
+    <a class="navbutton" href="/apps.html"><li> <img id="apps" class="navimg" src=""><p1>ㅤ</p1><p1> apps</p1></li></a>
+    <a class="navbutton" href="/proxy.html"><li> <img id="proxy" class="navimg" src=""><p1>ㅤ</p1><p1 class="navtext"> Proxy</p1></li></a>
+    <a class="settingsimg" href="/settings.html"><li> <img id="settings" class="navimg settings" src=""></li></a>
     <a class="github" href="https://github.com/Xenapsis-dev/Xenapsis-V3-Bare"><li><p1>ㅤ</p1> Github</li></a>
 </ul>`
 
